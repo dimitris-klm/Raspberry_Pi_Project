7187c7713dafa85fbb17ce2f3d6cf5185ecc21d2
Dimitris Kalamaras <41650605+dimitris-klm@users.noreply.github.com>
2018-08-25 15:08:49 +0300

    Add files via upload

---
+++ Pi_Camera.py
@@ -0,0 +1,29 @@
+import picamera
+from time import sleep
+import datetime
+import base64
+
+def take_picture():
+    # First we need to take and save the picture
+    with picamera.PiCamera() as camera:
+        dt = datetime.datetime.now().strftime('%Y%m%d%H%M')
+        camera.start_preview()
+        sleep(1)
+        camera.resolution = (640,480)
+		# Save the image locally including the date and time
+        full_image_path = '<full_image_path>' + dt + '.jpg'
+        camera.capture(full_image_path)
+        camera.stop_preview()
+        sleep(0.5)
+
+    # Now we encode the picture that we took to get the binary file
+    image = open(full_image_path, 'rb')
+    image_read = image.read()
+    image_64_encode = base64.encodestring(image_read)
+
+    return image_64_encode
+
+if __name__ == "__main__":
+    print "Taking picture"
+    print take_picture()
+    print "Picture taken."
